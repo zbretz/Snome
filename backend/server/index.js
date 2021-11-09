a8d2e3f6239a176ec111709ad4454a6c9ce6d4cb
Zach <zachary.bretz@gmail.com>
2021-11-09 12:35:53 -0700

    setting up directory structure

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = require('./routes.js');
+const router = require('./routes');
 
 const app = express();
 const port = 3000;
--- backend/server/routes/index.js
+++ backend/server/routes/index.js
@@ -1,5 +1,29 @@
-/* bundle model queries for export */
+// const controller = require('./controllers');
+// const router = require('express').Router();
 
-exports.get = require('./get.js');
-exports.post = require('./post.js');
-exports.put = require('./put.js');
+// /* define API url to handler mappings here */
+
+// /* FOR BAREBONES TESTING ONLY */
+// router.get('/snome', controller.get.getAll);
+// // router.post('/snomes', controller.post.create);
+
+// module.exports = router;
+
+
+
+// exports.snomes = require('./snomes.js');
+// exports.users = require('./users.js');
+
+var express = require('express');
+var router = express.Router();
+
+var snomes = require('./snomes.js')
+var users = require('./users.js')
+
+router.use('snomes', snomes);
+// app.use('quotes', users);
+
+
+// exports.get = require('./get');
+// exports.post = require('./post');
+// exports.put = require('./put');
