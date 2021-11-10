f4987dddad9e0cde4c47a2e900801bab9887d980
Zach <zachary.bretz@gmail.com>
2021-11-10 08:36:58 -0700

    Removed unnecessary routes

--- backend/server/routes/snome.routes.js
+++ backend/server/routes/snome.routes.js
@@ -1,30 +1,20 @@
 const snomes = require("../controllers/snome.controller.js");
 var router = require("express").Router();
 
-// module.exports =  {
+// Create a new Snome
+router.post("/", snomes.create);
 
+// Retrieve all Snomes
+router.get("/", snomes.findAll);
 
-  // Create a new Snome
-  router.post("/", snomes.create);
+// Retrieve a single Snome with id
+router.get("/:id", snomes.findOne);
 
-  // Retrieve all Snomes
-  router.get("/", snomes.findAll);
+// Update a Snome with id
+router.put("/:id", snomes.update);
 
-  // Retrieve all published Snomes
-  router.get("/published", snomes.findAllPublished);
+// Delete a Snome with id
+router.delete("/:id", snomes.delete);
 
-  // Retrieve a single Snome with id
-  router.get("/:id", snomes.findOne);
-
-  // Update a Snome with id
-  router.put("/:id", snomes.update);
-
-  // Delete a Snome with id
-  router.delete("/:id", snomes.delete);
-
-  // Create a new Snome
-  router.delete("/", snomes.deleteAll);
-
-// };
 
 module.exports = router;
